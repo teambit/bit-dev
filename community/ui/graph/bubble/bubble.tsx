e2b5348b5bdd5d7faa925e0a63e6f98eefdbb7d3
Ran Mizrahi <me@ranmizrahi.com>
2021-09-19 13:50:22 +0300

    fixed errors

--- community/ui/graph/bubble/bubble.tsx
+++ community/ui/graph/bubble/bubble.tsx
@@ -35,7 +35,7 @@ export function Bubble({ className, icon, forceActive, color = '#EDEDED', ...res
   return (
     <div className={classNames(styles.bubble, className, forceActive ? styles.active : '')} {...rest}>
       <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M13.1937 1.83337C8.09096 4.06476 3.9981 8.17609 1.79077 13.2917" stroke={color} stroke-width="2" stroke-linecap="round"/>
+        <path d="M13.1937 1.83337C8.09096 4.06476 3.9981 8.17609 1.79077 13.2917" stroke={color} strokeWidth="2" strokeLinecap="round"/>
       </svg>
       {icon ? <Image src={icon} className={styles.icon} /> : ''}
     </div>
