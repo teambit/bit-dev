c293e8410d1341082dbfb8d8fe95a01bd44dc272
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2021-09-24 14:51:54 +0200

    add community env, remove bitdev env

---
+++ community/envs/community-react/index.ts
@@ -0,0 +1,10 @@
+import {
+  CommunityReactAspect,
+  CommunityReactConfig
+} from './community-react.aspect';
+
+export type { CommunityReactMain } from './community-react.main.runtime';
+export type { CommunityReactPreview } from './community-react.preview.runtime';
+
+export default CommunityReactAspect;
+export { CommunityReactAspect, CommunityReactConfig };
