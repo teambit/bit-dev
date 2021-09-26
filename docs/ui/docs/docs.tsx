9ab22538a63227b18f79026ae72246d16ea87e1a
oded <oded@bit.dev>
2021-09-26 18:47:55 +0300

    added split pane to docs

--- docs/ui/docs/docs.tsx
+++ docs/ui/docs/docs.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import classNames from 'classnames';
 import { DocsRoute } from './docs-route';
 import { Switch, Route, useRouteMatch } from 'react-router-dom';
 import { Sidebar } from '@teambit/docs.ui.sidebar';
 import { DocPage } from '@teambit/docs.ui.pages.doc-page';
+import { Collapser } from '@teambit/ui-foundation.ui.buttons.collapser';
+import {
+  SplitPane,
+  SplitPaneProps,
+  Pane,
+  Layout,
+} from '@teambit/base-ui.surfaces.split-pane.split-pane';
+import { HoverSplitter } from '@teambit/base-ui.surfaces.split-pane.hover-splitter';
 import styles from './docs.module.scss';
 import { DocsRoutes } from './routes';
 
@@ -11,31 +19,47 @@ export type DocsProps = {
   /**
    * a text to be rendered in the component.
    */
-  routes: DocsRoute[]
-} & React.HTMLAttributes<HTMLDivElement>;
+  routes: DocsRoute[];
+} & SplitPaneProps;
 
 export function Docs({ routes, ...rest }: DocsProps) {
   const docRoutes = DocsRoutes.from(routes);
+  const [isSidebarOpen, handleSidebarToggle] = useReducer((x) => !x, true);
+  const sidebarOpenness = isSidebarOpen ? Layout.row : Layout.right;
   const { path } = useRouteMatch();
 
   return (
-    <div className={classNames(styles.docs)} {...rest}>
-      <Sidebar 
-        className={styles.sidebar} 
-        paths={docRoutes.getSideBarPaths()} 
-        onSelect={(id) => console.log(id)}
-      />
-      <div className={styles.content}>
+    <SplitPane
+      {...rest}
+      className={styles.main}
+      size={264}
+      layout={sidebarOpenness}
+    >
+      <Pane className={styles.sidebar}>
+        <Sidebar
+          paths={docRoutes.getSideBarPaths()}
+          onSelect={(id) => console.log(id)}
+        />
+      </Pane>
+      <HoverSplitter className={styles.splitter}>
+        <Collapser
+          isOpen={isSidebarOpen}
+          onMouseDown={(e) => e.stopPropagation()} // avoid split-pane drag
+          onClick={handleSidebarToggle}
+          tooltipContent={`${isSidebarOpen ? "Hide" : "Show"} side panel`}
+        />
+      </HoverSplitter>
+      <Pane className={styles.content}>
         <Switch>
           {routes.map((route, key) => {
-            return <Route key={key} path={`${path}/quick-start`}>
-              <DocPage title={route.title}>
-                {route.component}
-              </DocPage>
-            </Route>            
+            return (
+              <Route key={key} path={`${path}/quick-start`}>
+                <DocPage title={route.title}>{route.component}</DocPage>
+              </Route>
+            );
           })}
         </Switch>
-      </div>
-    </div>
+      </Pane>
+    </SplitPane>
   );
 }
