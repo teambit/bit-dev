de3e09ddb55fecdcd3af15b06117ef3180da881b
oded <oded@bit.dev>
2021-09-30 16:42:42 +0300

    deparated data and mock to a component in enity namespace

---
+++ community/entity/compnent-distribution-graph/component.ts
@@ -0,0 +1,57 @@
+import { ComponentID } from "@teambit/component-id";
+import type {EdgeProps} from '@teambit/community.ui.graph.edge'
+
+export type ComponentProps = {
+  id: string;
+  dependencies: string[];
+  position?: 'top' | 'top-right' | 'right' | 'bottom-right' | 'bottom' | 'bottom-left' | 'left' | 'top-left';
+  preview?: string;
+  edges?: Edge;
+};
+
+export type Edge = Record<string, Omit<EdgeProps, 'start' | 'end'>>;
+
+export class Component {
+  constructor(
+    /**
+     * ID of the component presented in the component card.
+     */
+    readonly id: ComponentID,
+
+    /**
+     * dependencies to link to.
+     */
+    readonly dependencies: ComponentID[],
+
+    /**
+     * position of the component card in cell.
+     */
+    readonly position: string,
+    /**
+     * the preview to be displayed in the card.
+     */
+    readonly preview?: string,
+    /**
+     * a list of egdes to override with custom styles
+     */
+    readonly edges?: Edge
+  ) {}
+
+  static fromObject(component: ComponentProps) {
+    return new Component(
+      ComponentID.fromString(component.id),
+      component.dependencies.map((dep) => ComponentID.fromString(dep)),
+      component.position,
+      component.preview,
+      component.edges,
+    );
+  }
+
+  toObject() {
+    return {
+      id: this.id.toString(),
+      dependencies: this.dependencies.map((dep) => dep.toString()),
+      position: this.position,
+    };
+  }
+}
---
+++ community/entity/compnent-distribution-graph/index.ts
@@ -0,0 +1,6 @@
+export { Component } from './component';
+export type { ComponentProps, Edge } from './component';
+
+export { ComponentGraph } from './component-graph';
+
+export { componentsMock } from './component-distribution.mock';
