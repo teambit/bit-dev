fab57d6b0cfa560520ca81cf6d434db0f7a8530f
Ran Mizrahi <me@ranmizrahi.com>
2021-10-03 14:00:51 +0300

    naming components

---
+++ component/content/naming/index.ts
@@ -0,0 +1,1 @@
+export { default as Naming, default } from './naming.mdx';
---
+++ component/content/naming/naming.composition.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Naming } from './index';
+
+export const BasicNaming = () => (
+  <Naming />
+);
