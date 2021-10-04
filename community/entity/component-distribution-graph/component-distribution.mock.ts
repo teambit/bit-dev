09029343402a5efa4779fd101b3b9146321c84e3
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2021-10-04 15:34:59 +0200

    add card info for graph
    correct spelling of component dir
    mock only added. types need to be added

--- community/entity/component-distribution-graph/component-distribution.mock.ts
+++ community/entity/component-distribution-graph/component-distribution.mock.ts
@@ -6,56 +6,77 @@ export const componentsMock = () => {
       dependencies: [],
       position: 'top',
       preview: 'https://static.bit.dev/brands/logo-react.svg',
+      version: '2.0.1',
+      description: 'A Button component for buttons and elements that look like buttons',
+      envIcon: 'https://static.bit.dev/brands/logo-react.svg'
     },
     {
       id: 'ecommerce/ui/store-hero',
       dependencies: ['base-ui/ui/button'],
       position: 'bottom',
       preview: 'https://static.bit.dev/brands/logo-angular.svg',
+      version: '0.0.11',
+      description: 'A hero component for the store',
+      envIcon: 'https://static.bit.dev/brands/logo-react.svg'
     },
     {
       id: 'ecommerce/ui/header',
       dependencies: [],
       preview: 'https://static.bit.dev/brands/logo-nodejs.svg',
+      version: '1.0.12',
+      description: 'A header component',
+      envIcon: 'https://static.bit.dev/brands/logo-react.svg'
     },
     {
-      id: 'shoe-store/ui/pages/home-page',
+      id: 'shoe-store/ui/pages/home',
       dependencies: [
         'ecommerce/ui/header',
         'shoe-store/ui/shoes/shoes-card-grid',
-        'shoe-store/home-page-to-store-hero' // conneting node to create the curved edge effect
+        'shoe-store/home-page-to-store-hero' // connecting node to create the curved edge effect
       ],
       position: 'bottom',
       edges: {
         'shoe-store/home-page-to-store-hero': {
           showHead: false,
           startAnchor: 'top',
-          endAnchor: 'right',
-        },
+          endAnchor: 'right'
+        }
       },
       preview:
         'https://bitsrc.imgix.net/8906f31bf4ae987413d3fdc1171be928f6b16e59.png?fit=scale&w=70&h=70',
+      version: '1.0.16',
+      description: 'A page component for rending contents of the home page',
+      envIcon: 'https://static.bit.dev/brands/logo-react.svg'
     },
     {
-      id: 'ecommerce/ui/product/price',
+      id: 'ecommerce/ui/product/currency',
       dependencies: [],
       position: 'top',
       preview: 'https://static.bit.dev/brands/logo-react.svg',
+      version: '0.0.1',
+      description: 'A currency component',
+      envIcon: 'https://static.bit.dev/brands/logo-react.svg'
     },
     {
       id: 'shoe-store/ui/shoes/shoes-card',
       dependencies: [
         'shoe-store/entity/shoes',
         'ecommerce/ui/product/price',
-        'base-ui/ui/button',
+        'base-ui/ui/button'
       ],
-      preview: 'https://static.bit.dev/brands/logo-nodejs.svg',
+      preview: 'https://static.bit.dev/brands/logo-nodejs.svg'
+      version: '1.0.11',
+      description: 'A product card for shoes',
+      envIcon: 'https://static.bit.dev/brands/logo-react.svg'
     },
     {
       id: 'shoe-store/entity/shoes',
       dependencies: [],
       position: 'top',
-      preview: 'https://static.bit.dev/brands/logo-angular.svg',
+      preview: 'https://static.bit.dev/brands/logo-angular.svg'
+      version: '1.0.4',
+      description: 'A shoes data structure',
+      envIcon: 'https://static.bit.dev/brands/logo-node.svg'
     },
     {
       id: 'shoe-store/ui/shoes/shoes-card-grid',
@@ -68,6 +89,9 @@ export const componentsMock = () => {
       },
       preview:
         'https://bitsrc.imgix.net/8906f31bf4ae987413d3fdc1171be928f6b16e59.png?fit=scale&w=70&h=70',
-    },
+      version: '1.0.11',
+      description: 'A card grid for the product cards for shoes',
+      envIcon: 'https://static.bit.dev/brands/logo-react.svg'
+    }
   ]);
 };
