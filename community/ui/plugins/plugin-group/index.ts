b83d2e3828f2274b225b1f5a4c40a12b65fca2c9
Josh Kuttler <yoshuakuttler@gmail.com>
2021-10-19 21:53:40 +0300

    remove plugins group component

--- community/ui/plugins/plugin-group/index.ts
+++
@@ -1,3 +0,0 @@
-// Use Component Card Group component from explorer instead of this.
-export { PluginGroup } from './plugin-group';
-export type { PluginGroupProps } from './plugin-group';
--- community/ui/plugins/plugin-group/plugin-group.spec.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { BasicPluginGroup } from './plugin-group.composition';
-
-it('should render with the correct text', () => {
-  const { getByText } = render(<BasicPluginGroup />);
-  const rendered = getByText('hello from PluginGroup');
-  expect(rendered).toBeTruthy();
-});
