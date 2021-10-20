0290dc42472fda93b187010f74ba5aa949a584ae
Ran Mizrahi <me@ranmizrahi.com>
2021-10-20 13:54:41 +0300

    replaced mdx-layout

--- compilation/content/compiling-during-development/compiling-during-development.composition.tsx
+++ compilation/content/compiling-during-development/compiling-during-development.composition.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ThemeCompositions } from '@teambit/documenter.theme.theme-compositions';
-import { MDXLayout } from '@teambit/ui.mdx-layout';
+import { MDXLayout } from '@teambit/mdx.ui.mdx-layout';
 import { CompilingDuringDevelopment } from './index';
 
 export const BasicCompilingDuringDevelopment = () => {
