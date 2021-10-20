bb7461e5f96e9f315ec6844d9fb62032e05a8925
Ran Mizrahi <me@ranmizrahi.com>
2021-10-20 23:07:32 +0300

    bumped

--- design/ui/layouts/sections/section-group/section-group.composition.tsx
+++ design/ui/layouts/sections/section-group/section-group.composition.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { SectionGroup } from './section-group';
 
 export const BasicSectionGroup = () => (
-  <SectionGroup text="hello from SectionGroup" />
+  <SectionGroup />
 );
