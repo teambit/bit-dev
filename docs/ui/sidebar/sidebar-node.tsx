84b81b5a2c35413e0040959fe1998b78d243e126
oded <oded@bit.dev>
2021-10-21 19:07:16 +0300

    fixed sidebar collapse issues

--- docs/ui/sidebar/sidebar-node.tsx
+++ docs/ui/sidebar/sidebar-node.tsx
@@ -15,21 +15,24 @@ export type SidebarNodeProps = {
 } & TreeNodeComponentProps<any>;
 
 export function SidebarNode(props: SidebarNodeProps) {
-  const isActive = window?.location?.pathname?.includes(props.node.id)
+  const currentPath = location?.pathname;
+  
   if (!props.node.children) {
+    const isNodeActive = currentPath === props.node.payload.path
     return (
       // TODO: migrate to use the new base-react link with React Router.
-        <TreeNode 
-          node={{ id: props.node.payload.title }}
-          icon={props.node.payload?.icon}
-          depth={props.depth}
-          // TODO - navLink should show up as active by itself
-          isActive={isActive}
-          href={props.node.payload?.path}
+      <TreeNode 
+      node={{ id: props.node.payload.title }}
+      icon={props.node.payload?.icon}
+      depth={props.depth}
+      // TODO - navLink should show up as active by itself
+      isActive={isNodeActive}
+      href={props.node.payload?.path}
       />
-    );
-  }
-
+      );
+    }
+    
+  const isFolderActive = currentPath.includes(props.node.id)
   return (
     <FolderTreeNode
       node={{
@@ -37,7 +40,7 @@ export function SidebarNode(props: SidebarNodeProps) {
         children: props.node.children,
         payload: {
           ...props.node.payload,
-          open: isActive || props.node.payload.open,
+          open: isFolderActive || props.node.payload.open,
         },
       }}
       depth={props.depth}
