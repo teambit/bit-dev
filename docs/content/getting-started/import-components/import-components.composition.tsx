c0c70224514d6f10953543b687e100ee56f85633
Guysaar <guy@bit.dev>
2021-10-25 12:01:32 +0300

    update

--- docs/content/getting-started/import-components/import-components.composition.tsx
+++ docs/content/getting-started/import-components/import-components.composition.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { ThemeCompositions } from '@teambit/documenter.theme.theme-compositions';
 import { MDXLayout } from '@teambit/mdx.ui.mdx-layout';
-import { InstallComponents } from './index';
+import { ImportComponents } from './';
 
 export const BasicCreateWorkspace = () => (
   <ThemeCompositions>
     <MDXLayout>
-      <InstallComponents />
+      <ImportComponents />
     </MDXLayout>
   </ThemeCompositions>
 );
