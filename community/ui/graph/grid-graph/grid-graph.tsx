52e8b6d7d1a2edb53edf2d6f6360d8c41c79a974
oded <oded@bit.dev>
2021-11-02 20:45:51 +0200

    responsive graph - initial commit

--- community/ui/graph/grid-graph/grid-graph.tsx
+++ community/ui/graph/grid-graph/grid-graph.tsx
@@ -1,4 +1,6 @@
-import React, { ComponentType } from 'react';
+import React, { ComponentType, useState, useEffect } from 'react';
+import useDimensions from 'react-use-dimensions';
+import { useDebounce } from 'use-debounce';
 import { ComponentID } from '@teambit/component-id';
 import classNames from 'classnames';
 import { GridNode, DependencyEdge } from '@teambit/community.entity.graph.grid-graph';
@@ -7,96 +9,132 @@ import { DefaultNode } from './default-node';
 import styles from './grid-graph.module.scss';
 
 export type GridItemProps = {
-id?: string,
-dependencies?: ComponentID[]
-icon?: string,
-row?: number,
-col?: number,
-position?: 'top' | 'top-right' | 'right' | 'bottom-right' | 'bottom' | 'bottom-left' | 'left' | 'top-left';
+  id?: string;
+  dependencies?: ComponentID[];
+  icon?: string;
+  row?: number;
+  col?: number;
+  position?: 'top' | 'top-right' | 'right' | 'bottom-right' | 'bottom' | 'bottom-left' | 'left' | 'top-left';
 } & React.HTMLAttributes<HTMLDivElement>;
 
 export type GraphNodeProps<T> = {
-  node: GridNode<T>
+  node: GridNode<T>;
 } & React.HTMLAttributes<HTMLDivElement>;
 
 export type GraphEdgeProps<T> = {
   node: GridNode<T>;
-  dependency: DependencyEdge
+  dependency: DependencyEdge;
 };
 
 export type GridGraphProps<TN = {}, TE = {}> = {
-  nodes: GridNode<TN>[],
-  nodeClassName?: string
-  Node?: ComponentType<GraphNodeProps<TN>>,
-  Edge?: ComponentType<GraphEdgeProps<TE>>
+  nodes: GridNode<TN>[];
+  nodeClassName?: string;
+  Node?: ComponentType<GraphNodeProps<TN>>;
+  Edge?: ComponentType<GraphEdgeProps<TE>>;
+  graphSize?: 'xs' | 'sm' | 'md' | 'l' | 'lg' | 'xl' | 'xxl';
 } & React.HTMLAttributes<HTMLDivElement>;
 
-export function GridGraph({ children, nodeClassName, nodes, Node = DefaultNode, Edge = DefaultEdge, className, ...rest }: GridGraphProps) {
+export function GridGraph({
+  children,
+  nodeClassName,
+  nodes,
+  Node = DefaultNode,
+  Edge = DefaultEdge,
+  graphSize,
+  className,
+  ...rest
+}: GridGraphProps) {
+  const [currentBreakpoint, setBreakpoint] = useState<string | null>('xxl');
+  const [ref, { width }] = useDimensions();
+  const debouncedSize = useDebounce(width, 300, { leading: true });
+  useEffect(() => {
+    if (debouncedSize[0] > 1200) {
+      setBreakpoint('xxl');
+    }
+    if (debouncedSize[0] < 1200) {
+      setBreakpoint('md');
+    }
+  }, [debouncedSize]);
+
   return (
-    <div className={classNames(styles.gridGraph, className)} {...rest}>
-        {nodes.map((node) => {
-          const id = getValidId(node.id.toString({ignoreVersion: true}))
-          const cell = getCell(node.row, node.col)
-          const bubblePosition = node.position && positions[node.position];
-          return (
-            <div key={node.id.toString()} className={nodeClassName} style={{ ...cell, ...bubblePosition }}>
-              <Node id={node.attrId} key={id} node={node} />
-              
-              {node.dependencies.map((dependency) => {
-                return <Edge key={`${node.attrId}->${dependency.attrId}`} node={node} dependency={dependency} />
-              })}
-            </div>
-          )
-        })}
-        {children}
+    <div ref={ref} className={classNames(styles.gridGraph, className)} {...rest}>
+      {nodes.map((node) => {
+        const id = getValidId(node.id.toString({ ignoreVersion: true }));
+        const cell = getCell(node, currentBreakpoint);
+        // console.log("cell", cell)
+        const bubblePosition = node.position && positions[node.position];
+        return (
+          <div key={node.id.toString()} className={nodeClassName} style={{ ...cell, ...bubblePosition }}>
+            <Node id={node.attrId} key={id} node={node} />
+
+            {node.dependencies.map((dependency) => {
+              return <Edge key={`${node.attrId}->${dependency.attrId}`} node={node} dependency={dependency} />;
+            })}
+          </div>
+        );
+      })}
+      {children}
     </div>
   );
 }
 
 export const positions = {
-  'top': {
+  top: {
     alignSelf: 'start',
     justifySelf: 'center',
   },
-   'top-right': {
-     alignSelf: 'start',
-     justifySelf: 'end',
-   },
-   'right': {
-     alignSelf: 'center',
-     justifySelf: 'end',
-   },
-   'bottom-right': {
-     alignSelf: 'end',
-     justifySelf: 'end',
-   },
-   'bottom': {
-     alignSelf: 'end',
-     justifySelf: 'center',
-   },
-   'bottom-left': {
-     alignSelf: 'end',
-     justifySelf: 'end',
-   },
-   'left': {
-     alignSelf: 'center',
-     justifySelf: 'end',
-   },
-   'top-left': {
-     alignSelf: 'start',
-     justifySelf: 'end',
-   }
+  'top-right': {
+    alignSelf: 'start',
+    justifySelf: 'end',
+  },
+  right: {
+    alignSelf: 'center',
+    justifySelf: 'end',
+  },
+  'bottom-right': {
+    alignSelf: 'end',
+    justifySelf: 'end',
+  },
+  bottom: {
+    alignSelf: 'end',
+    justifySelf: 'center',
+  },
+  'bottom-left': {
+    alignSelf: 'end',
+    justifySelf: 'end',
+  },
+  left: {
+    alignSelf: 'center',
+    justifySelf: 'end',
+  },
+  'top-left': {
+    alignSelf: 'start',
+    justifySelf: 'end',
+  },
 };
 
 export function getValidId(id: string) {
   return id.replace(/[.\/]/g, '-');
 }
 
-export function getCell(row?: number, col?: number) {
+export function getCell<T>(node: GridNode<T>, graphSize?: string) {
+  // prevent flickering on first load
+  if (graphSize === null) {
+    return { display: 'none' };
+  }
+
+  // when specifying col and row to equal to null, it removed the node
+  if (graphSize && node?.sizes?.[graphSize]?.col === null && node?.sizes?.[graphSize]?.row === null) {
+    return { display: 'none' };
+  }
+
+  const col = (graphSize && node?.sizes?.[graphSize]?.col) || node.col;
+  const row = (graphSize && node?.sizes?.[graphSize]?.row) || node.row;
+
   return {
     gridColumnStart: col,
     gridColumnEnd: col,
     gridRowStart: row,
-    gridRowEnd: row
-  }
+    gridRowEnd: row,
+  };
 }
