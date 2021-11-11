b286c3fd8d9b239bfdc2c476416376d4651afa1b
Guysaar <guy@bit.dev>
2021-11-12 00:48:49 +0200

    fix

--- scope/content/hosting-scopes/hosting-scopes.composition.tsx
+++ scope/content/hosting-scopes/hosting-scopes.composition.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ThemeCompositions } from '@teambit/documenter.theme.theme-compositions';
-import { MDXLayout } from '@teambit/modules.mdx-loader';
+import { MDXLayout } from '@teambit/mdx.ui.mdx-layout';
 import { HostingScopes } from './index';
 
 export const BasicHostingScopes = () => {
