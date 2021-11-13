c390a36937f8e364ea9f53e4d35042260ce88d13
Ran Mizrahi <me@ranmizrahi.com>
2021-11-13 17:24:07 +0200

    improvements

--- community/apps/bit-dev/app.tsx
+++ community/apps/bit-dev/app.tsx
@@ -1,22 +1,14 @@
-import React, { useState, ReactNode } from 'react';
+import React, { useState } from 'react';
 import loadable from '@loadable/component';
 import { Switch, Route } from 'react-router-dom';
-import { ReactRouterRoutingProvider } from '@teambit/ui-foundation.ui.navigation.react-router.routing-adapter';
-import { SidebarProvider } from '@teambit/design.ui.sidebar.sidebar-context';
-
 import { Guides } from '@teambit/docs.ui.pages.guides';
 import { Header } from '@teambit/community.ui.header.header';
 import { Homepage } from '@teambit/community.ui.pages.homepage';
-import { ThemeCompositions } from '@teambit/documenter.theme.theme-compositions';
 import { NotFound } from '@teambit/community.ui.pages.errors.not-found';
 import { CommunityDocs } from '@teambit/docs.ui.community-docs';
 import { WideColumn } from '@teambit/base-ui.layout.page-frame';
-import { ComponentHighlighter } from '@teambit/react.ui.component-highlighter';
-import { RoutingProvider } from '@teambit/base-ui.routing.routing-provider';
 import { Footer, footerMock } from '@teambit/community.ui.footer.footer';
-
-import { legacyRouting } from './legacy-routing';
-import styles from './app.module.scss';
+import { AppContext } from './app-context';
 
 /**
  * Load pages dynamically to enable code splitting.
@@ -54,27 +46,5 @@ export function BitDevApp() {
       </WideColumn>
       {/* footer component */}
     </AppContext>
-  );
-}
-
-function AppContext({ children, showHighlighter }: { children?: ReactNode; showHighlighter?: boolean }) {
-  // TODO @Uri - remove the legacy RoutingProvider
-  return (
-    <RoutingProvider value={legacyRouting}>
-      <SidebarProvider>
-        <ReactRouterRoutingProvider useBrowserRouter>
-          <ThemeCompositions>
-            <ComponentHighlighter
-              classes={{ label: styles.label, frame: styles.frame }}
-              placement="top"
-              style={{ border: 'none' }}
-              disabled={!showHighlighter}
-            >
-              {children}
-            </ComponentHighlighter>
-          </ThemeCompositions>
-        </ReactRouterRoutingProvider>
-      </SidebarProvider>
-    </RoutingProvider>
   );
 }
