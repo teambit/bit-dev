dc0d21c2d09fad935f87d40e905934174289cf40
Ran Mizrahi <me@ranmizrahi.com>
2021-11-16 13:39:49 +0200

    fix error

--- design/ui/navigation/link/nav-link.tsx
+++ design/ui/navigation/link/nav-link.tsx
@@ -5,9 +5,15 @@ import styles from './link.module.scss';
 
 export type { NavLinkProps };
 
-export const NavLink = forwardRef<HTMLAnchorElement, NavLinkProps>((
+export const NavLink = forwardRef<HTMLAnchorElement, NavLinkProps>(
   ({ className, activeClassName, ...rest }: NavLinkProps, ref) => {
     return (
-      <BaseNavLink className={classNames(styles.link, className)} activeClassName={activeClassName} {...rest} ref={ref} />
+      <BaseNavLink
+        className={classNames(styles.link, className)}
+        activeClassName={activeClassName}
+        {...rest}
+        ref={ref}
+      />
     );
-  });
+  }
+);
