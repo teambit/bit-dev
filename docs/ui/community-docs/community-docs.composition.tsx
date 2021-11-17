0c32f02eaf00dd41efaa9c3f1d7289eba8ade8bd
Josh Kuttler <yoshuakuttler@gmail.com>
2021-11-17 18:35:40 +0200

    add compositions and docs to community-docs

--- docs/ui/community-docs/community-docs.composition.tsx
+++ docs/ui/community-docs/community-docs.composition.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
 import { CommunityDocs } from './community-docs';
 
-export const BasicCommunityDocs = () => <CommunityDocs />;
+export const BasicCommunityDocs = () => (
+  <CommunityDocs baseUrl="https://teambit-community-apps-bit-dev.netlify.app/docs" />
+);
