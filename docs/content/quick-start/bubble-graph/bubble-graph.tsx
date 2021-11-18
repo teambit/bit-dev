ec783661880f2120053d798a8c608381bb459b38
oded <oded@bit.dev>
2021-11-18 16:54:30 +0200

    design

--- docs/content/quick-start/bubble-graph/bubble-graph.tsx
+++ docs/content/quick-start/bubble-graph/bubble-graph.tsx
@@ -7,16 +7,17 @@ import styles from './bubble-graph.module.scss';
 
 export function InnovatorGraph() {
   return (
-    <div>
-      <BubbleGraph className={styles.innovatorGrid} nodeClassName={styles.bubbleWrapper} nodes={demoGraph}>
-        <Label color="#0984E3" className={styles.designLabel}>
-          Design
-        </Label>
-        <Label className={styles.innovatorsLabel}>Innovators</Label>
-        <div id="teambit-bubble-graph-ui-connecter" className={styles.connecter} />
-        <Cloud className={styles.cloud} />
-        <Cloud className={styles.topCloud} />
-      </BubbleGraph>
-    </div>
+    // <div>
+    <BubbleGraph className={styles.innovatorGrid} nodeClassName={styles.bubbleWrapper} nodes={demoGraph}>
+      <div className={styles.dotsBackground} />
+      <Label color="#0984E3" className={styles.designLabel}>
+        Design
+      </Label>
+      <Label className={styles.innovatorsLabel}>Innovators</Label>
+      <div id="teambit-bubble-graph-ui-connecter" className={styles.connecter} />
+      <Cloud className={styles.cloud} />
+      <Cloud className={styles.topCloud} />
+    </BubbleGraph>
+    // </div>
   );
 }
