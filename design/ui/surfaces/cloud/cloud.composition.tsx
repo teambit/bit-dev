c189c481c2000fe426846fc78e1fd9aec7317e11
oded <oded@bit.dev>
2021-11-18 14:32:39 +0200

    cloud components and labels

---
+++ design/ui/surfaces/cloud/cloud.composition.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { Cloud } from './cloud';
+
+export const BasicCloud = () => <Cloud />;
--- docs/content/quick-start/bubble-graph/bubble-graph.tsx
+++ docs/content/quick-start/bubble-graph/bubble-graph.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BubbleGraph } from '@teambit/community.ui.graph.bubble-graph';
+import { Cloud } from '@teambit/design.ui.surfaces.cloud';
+import { Label } from '@teambit/base-ui.elements.label';
 import { demoGraph } from '../demo-graph';
 import styles from './bubble-graph.module.scss';
 
@@ -7,8 +9,12 @@ export function InnovatorGraph() {
   return (
     <div>
       <BubbleGraph className={styles.innovatorGrid} nodeClassName={styles.bubbleWrapper} nodes={demoGraph}>
-        <div className={styles.cloud} />
-        <div className={styles.topCloud} />
+        <Label color="#0984E3" className={styles.designLabel}>
+          Design
+        </Label>
+        <Label className={styles.innovatorsLabel}>Innovators</Label>
+        <Cloud className={styles.cloud} />
+        <Cloud className={styles.topCloud} />
       </BubbleGraph>
     </div>
   );
