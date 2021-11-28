9b4b34d225b806def140230d67b076ea5c916f86
oded <oded@bit.dev>
2021-11-28 17:32:35 +0200

    doc routes lazy

--- docs/ui/community-docs/primary-routes.tsx
+++ docs/ui/community-docs/primary-routes.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import loadable from '@loadable/component';
+import { lazy } from '@loadable/component';
 import { DocsRoute } from '@teambit/docs.entities.docs-routes';
 
 /* Quick Start */
-const QuickStart = loadable(() => import('@teambit/docs.content.quick-start'));
-const ThinkingInComponents = loadable(() => import('@teambit/docs.content.thinking-in-components'));
+const QuickStart = lazy(() => import('@teambit/docs.content.quick-start'));
+const ThinkingInComponents = lazy(() => import('@teambit/docs.content.thinking-in-components'));
 
 export const primaryRoutes: DocsRoute[] = [
   {
