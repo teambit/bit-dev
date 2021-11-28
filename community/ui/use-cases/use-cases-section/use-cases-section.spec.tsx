6d3c25824557f2c2bb329916be334ccfdc3d679e
Josh Kuttler <yoshuakuttler@gmail.com>
2021-11-28 11:59:37 +0200

    update use-cases-section, add links in the mock data and add tests

--- community/ui/use-cases/use-cases-section/use-cases-section.spec.tsx
+++ community/ui/use-cases/use-cases-section/use-cases-section.spec.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { BasicUseCasesSection } from './use-cases-section.composition';
 
-it.skip('should render with the correct text', () => {
+it('should render with the correct title text', () => {
   const { getByText } = render(<BasicUseCasesSection />);
-  const rendered = getByText('hello from UseCasesSection');
+  const rendered = getByText('These are no longer a challenge');
+
+  expect(rendered).toBeTruthy();
+});
+it('should render with the correct link text', () => {
+  const { getByText } = render(<BasicUseCasesSection />);
+  const rendered = getByText('See more use cases');
+
   expect(rendered).toBeTruthy();
 });
