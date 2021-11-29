ad874ef7aafdf09f1b1bf4997f897850bc7f26f5
oded <oded@bit.dev>
2021-11-29 21:18:06 +0200

    added mutation observer to check when mdx content has received

--- docs/ui/pages/doc-page/doc-page.tsx
+++ docs/ui/pages/doc-page/doc-page.tsx
@@ -44,22 +44,34 @@ export type DocPageProps = {
 const docSelectors = '.docs-heading h1, .docs-heading h2, .docs-heading h3';
 
 const components = mdxComponents('/docs', 'docs-heading');
-// const scrollToRef = (ref) => {
-//   return window.scrollTo(0, -ref.current.offsetTop);
-// };
+const scrollToRef = (ref) => {
+  return window.scrollTo(0, -ref.current.offsetTop);
+};
 
 export function DocPage({ title, description, nextPage, children, baseUrl = '/docs', plugins }: DocPageProps) {
   const myRef = useRef(null);
   const contentRef = useRef() as React.MutableRefObject<HTMLDivElement>;
-  const [showNextPage, setNextPage] = useState(false);
-  // const executeScroll = () => scrollToRef(myRef);
+  const [loaded, setLoaded] = useState(false);
+
   const pageDescription = description || `Documentation page for ${title} - Bit.`;
 
   useEffect(() => {
-    setTimeout(() => {
-      setNextPage(true); // hides next page component until mdx data loads. should also be fixed by ssr
-    }, 300);
-  }, [window.location.pathname]);
+    const targetNode = contentRef.current;
+
+    const config = {
+      attributes: true,
+      childList: true,
+      characterData: true,
+    };
+    const callback = () => {
+      setLoaded(true);
+      scrollToRef(myRef);
+    };
+    const observer = new MutationObserver(callback);
+    if (targetNode instanceof HTMLElement) {
+      observer.observe(targetNode, config);
+    }
+  }, [contentRef?.current]);
 
   // @TODO @josh remove when ssr is working
   useEffect(() => {
@@ -72,7 +84,7 @@ export function DocPage({ title, description, nextPage, children, baseUrl = '/do
   }, [window?.location.hash]);
 
   return (
-    <Suspense fallback={<div>loading!!!</div>}>
+    <Suspense fallback={<div />}>
       <Page title={`${title} | Bit`} description={pageDescription} className={styles.docsPage}>
         <div ref={myRef} id="content" className={styles.content}>
           <MDXLayout components={components}>
@@ -82,7 +94,7 @@ export function DocPage({ title, description, nextPage, children, baseUrl = '/do
           </MDXLayout>
           {/* <Panel className={styles.rightPanel} plugins={plugins?.flatMap((plugin) => plugin.right)} />
           <Panel className={styles.bottomPanel} plugins={plugins?.flatMap((plugin) => plugin.bottom)} /> */}
-          {nextPage && showNextPage && (
+          {nextPage && loaded && (
             <NextPage
               className={styles.next}
               title={nextPage.title}
@@ -92,12 +104,14 @@ export function DocPage({ title, description, nextPage, children, baseUrl = '/do
           )}
         </div>
 
-        <TableOfContent
-          title="on this page"
-          className={styles.tableOfContent}
-          rootRef={contentRef}
-          selectors={docSelectors}
-        />
+        {loaded && (
+          <TableOfContent
+            title="on this page"
+            className={styles.tableOfContent}
+            rootRef={contentRef}
+            selectors={docSelectors}
+          />
+        )}
       </Page>
     </Suspense>
   );
