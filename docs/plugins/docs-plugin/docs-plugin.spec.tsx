9259107129a04110357bcdcbe7cda118cd952749
Ran Mizrahi <me@ranmizrahi.com>
2021-12-02 17:20:20 +0200

    fixes and publish

--- docs/plugins/docs-plugin/docs-plugin.spec.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-// import { BasicDocsPlugin } from './docs-plugin.composition';
-
-it.skip('should render with the correct text', () => {
-  const { getByText } = render(<BasicDocsPlugin />);
-  const rendered = getByText('hello from DocsPlugin');
-  expect(rendered).toBeTruthy();
-});
