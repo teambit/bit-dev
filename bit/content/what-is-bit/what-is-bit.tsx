efcf47e6c0464ae6e03eeaf27e9c26c38197f443
Josh Kuttler <yoshuakuttler@gmail.com>
2021-12-06 15:35:54 +0200

    commented out not working code

--- bit/content/what-is-bit/what-is-bit.tsx
+++ bit/content/what-is-bit/what-is-bit.tsx
@@ -3,18 +3,18 @@ import WhatIsBitLong from './what-is-bit.mdx';
 import WhatIsBitShort from './what-is-bit-short.mdx';
 import WhatIsBitSmall from './what-is-bit-small.mdx';
 
-export enum Size {
-  SMALL,
-  MEDIUM,
-  LARGE
-}
+// export enum Size {
+//   SMALL,
+//   MEDIUM,
+//   LARGE
+// }
 
-export type WhatIsBitProps = {
-  size?: Size
-};
+// export type WhatIsBitProps = {
+//   size?: Size
+// };
 
-export function WhatIsBit({ size }: WhatIsBitProps) {
-  if (size === Size.SMALL) return <WhatIsBitSmall />;
-  if (size === Size.MEDIUM) return <WhatIsBitShort />
-  return <WhatIsBitLong />
+export function WhatIsBit({ size }) {
+  // if (size === Size.SMALL) return <WhatIsBitSmall />;
+  // if (size === Size.MEDIUM) return <WhatIsBitShort />
+  return <WhatIsBitLong />;
 }
