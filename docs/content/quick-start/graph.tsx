20324618308bcee070016aecc77c1b19ec4fdbae
Eden <eden@bit.dev>
2021-12-21 16:18:04 +0200

    comment out 'relations-graph'

--- docs/content/quick-start/graph.tsx
+++ docs/content/quick-start/graph.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { RelationsGraph } from '@teambit/graph.relations-graph';
+// import { RelationsGraph } from '@teambit/graph.relations-graph';
 
 export function WikiGraph() {
   return (
     <div style={{ width: '100%', height: 500 }}>
-      <RelationsGraph
+      {/* <RelationsGraph
         scopes={[{ id: 'teambit.wiki', isOpen: true }, { id: 'teambit.design' }, { id: 'teambit.docs' }]}
-      />
+      /> */}
     </div>
   );
 }
