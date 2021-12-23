08c3291d823a5a177ba177fd37fbe5e197b26eec
Ran Mizrahi <me@ranmizrahi.com>
2021-12-23 02:17:12 +0200

    fixed router

--- community/apps/bit-dev/app-context.tsx
+++ community/apps/bit-dev/app-context.tsx
@@ -1,12 +1,14 @@
 import React, { ReactNode } from 'react';
 import { ThemeCompositions } from '@teambit/documenter.theme.theme-compositions';
-import { RouterProvider } from '@teambit/community.ui.router.router-provider';
+import { RouterProvider } from '@teambit/base-react.navigation.router-context';
+// rename the below component to teambit.react/react-router/routing-provider.
+import { reactRouterAdapter } from '@teambit/ui-foundation.ui.navigation.react-router.routing-adapter';
 import { CommunityHighlighter } from '@teambit/community.ui.community-highlighter';
 
 export function AppContext({ children }: { children?: ReactNode }) {
   // TODO @Uri - remove the legacy RoutingProvider
   return (
-    <RouterProvider>
+    <RouterProvider value={reactRouterAdapter}>
       <ThemeCompositions>
         <CommunityHighlighter>{children}</CommunityHighlighter>
       </ThemeCompositions>
