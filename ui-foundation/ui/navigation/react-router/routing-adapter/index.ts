0395f9713abaa23ceaa1b9b6abce5575af81846a
Uri Kutner <urikutner@gmail.com>
2021-12-23 20:41:02 +0200

    fix

--- ui-foundation/ui/navigation/react-router/routing-adapter/index.ts
+++ ui-foundation/ui/navigation/react-router/routing-adapter/index.ts
@@ -1,7 +1,7 @@
 export { ReactRouterRoutingProvider, reactRouterAdapter, useLocation } from './react-routing-provider';
 export type { ReactRouterRoutingProviderProps } from './react-routing-provider';
 
-export { ReactRouterLink, ReactRouterNavLink } from './link';
-export type { NavLinkProps, LinkProps } from './link';
+export { ReactRouterLink, ReactRouterNavLink } from './implementation';
+export type { NavLinkProps, LinkProps } from './implementation';
 
-export { useNavigate } from './use-navigate';
+export { useNavigate } from './implementation/use-navigate';
