b725d3cdc1433d3e7ab5e9d4bd724c4f20548a0d
Uri Kutner <urikutner@gmail.com>
2021-12-27 14:59:40 +0200

    cleanup

--- ui-foundation/ui/navigation/react-router/routing-adapter/components/nav-link.tsx
+++ ui-foundation/ui/navigation/react-router/routing-adapter/components/nav-link.tsx
@@ -13,8 +13,6 @@ export const ReactRouterNavLink = forwardRef<HTMLAnchorElement, NavLinkProps>(fu
     href = '',
     state,
     active,
-
-    exact,
     children = null,
 
     // props we should not get, and should not pass to RR link:
@@ -30,6 +28,7 @@ export const ReactRouterNavLink = forwardRef<HTMLAnchorElement, NavLinkProps>(fu
   const isActive = useMemo(() => (active === undefined ? undefined : () => active), [active]);
 
   return (
+    // @ts-ignore (https://github.com/teambit/bit/issues/4401)
     <BaseNavLink to={to} isActive={isActive} ref={ref} {...rest}>
       {children}
     </BaseNavLink>
