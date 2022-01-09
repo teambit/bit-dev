4439db1aa89c56860dd4fe37b49a229b3e55ad67
Ran Mizrahi <me@ranmizrahi.com>
2022-01-09 12:52:36 +0200

    upgraded netlify

--- community/apps/bit-dev/bit-dev.react-app.ts
+++ community/apps/bit-dev/bit-dev.react-app.ts
@@ -1,12 +1,17 @@
 import { ReactAppOptions } from '@teambit/react';
-import { Netlify } from '@teambit/apps.netlify';
+import { NetlifyOptions, Netlify } from '@teambit/apps.netlify';
 
-const netlify = new Netlify(process.env.NETLIFY_AUTH_TOKEN as string, 'teambit-community-apps-bit-dev', 'yona');
+// const netlify = new Netlify(process.env.NETLIFY_AUTH_TOKEN as string, 'teambit-community-apps-bit-dev', 'yona');
+const netlify: NetlifyOptions = {
+  accessToken: process.env.NETLIFY_AUTH_TOKEN as string,
+  siteName: 'teambit-community-apps-bit-dev',
+  team: 'yona',
+};
 
 export const BitDevApp: ReactAppOptions = {
   name: 'bit-dev',
   entry: [require.resolve('./bit-dev.app-root')],
-  deploy: netlify.deploy.bind(netlify),
+  deploy: Netlify.deploy(netlify),
   prerender: {
     routes: ['/plugins'],
   },
