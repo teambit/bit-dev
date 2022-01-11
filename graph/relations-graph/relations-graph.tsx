e8bad53191254ec5b59e5605b6a2f29c88baf9a3
Uri Kutner <urikutner@gmail.com>
2022-01-11 18:02:44 +0200

    fix types

--- graph/relations-graph/relations-graph.tsx
+++ graph/relations-graph/relations-graph.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useMemo } from 'react';
 import { ComponentID } from '@teambit/component-id';
-import ReactFlow, { applyNodeChanges } from 'react-flow-renderer';
+import ReactFlow, { applyNodeChanges, OnLoad, OnNodesChange, Node, Edge } from 'react-flow-renderer';
 import { useRelationGraph } from '@teambit/graph.hooks.use-relation-graph';
 import { Scope as ScopeNode } from '@teambit/graph.nodes.scope';
 import { ScopeEdge } from '@teambit/graph.ui.graph.scope-edge';
@@ -33,14 +33,15 @@ function Graph({ scopes }: RelationsGraphProps) {
   const scopeIds = useMemo(() => scopes.map((scope) => scope.id), [scopes]);
   const openScopeIds = useMemo(() => scopes.filter((scope) => scope.isOpen).map((scope) => scope.id), [scopes]);
 
-  const [nodes, setNodes] = useState([]);
-  const [edges, setEdges] = useState([]);
+  const [nodes, setNodes] = useState<Node<{ label: any; isOpen: any }>[]>([]);
+  const [edges, setEdges] = useState<Edge[]>([]);
 
   const { graph, error, loading } = useRelationGraph(scopes);
 
   useEffect(() => {
     if (graph && graph.nodes && graph.edges) {
       (async () => {
+        // @ts-ignore
         const layout = await layoutGraph(graph.nodes, graph.edges, scopeIds, openScopeIds);
 
         const nds = layout.nodes.map((node) => {
@@ -48,19 +49,20 @@ function Graph({ scopes }: RelationsGraphProps) {
           return node;
         });
 
+        // @ts-ignore
         setNodes(nds);
         setEdges(layout.edges);
       })();
     }
   }, [graph, scopes, scopeIds, openScopeIds]);
 
-  const handleGraphLoad = (graph) => {
+  const handleGraphLoad: OnLoad = (graphInstance) => {
     setTimeout(() => {
-      graph.fitView();
+      graphInstance.fitView();
     }, 200);
   };
 
-  const handleNodesChange = (changes) => {
+  const handleNodesChange: OnNodesChange = (changes) => {
     setNodes((nds) => applyNodeChanges(changes, nds));
   };
 
