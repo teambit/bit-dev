8fe25c2c4d0941df61414b10c8d0cf162513945b
Kutner JS <urikutner@gmail.com>
2022-01-11 18:54:11 +0200

    Fix types (#197)

--- docs/ui/pages/doc-page/doc-page.spec.tsx
+++ docs/ui/pages/doc-page/doc-page.spec.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { BasicDocPage } from './doc-page.composition';
 
+// mock scrollTo used internally onMount by the docs page
+const scrollToMock = jest.fn();
+window.scrollTo = scrollToMock;
+
 it('should render with the correct text in the heading', () => {
   const { getByText } = render(<BasicDocPage />);
   const rendered = getByText('Create a new Workspace');
