45453c264e7286e3f8765bfb8ccaf8a926f2c88e
Uri Kutner <urikutner@gmail.com>
2022-01-20 13:33:28 +0200

    depreate legacy links

--- community/apps/bit-dev/app-context.tsx
+++ community/apps/bit-dev/app-context.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode } from 'react';
 import { ThemeCompositions } from '@teambit/documenter.theme.theme-compositions';
 import { RouterProvider } from '@teambit/base-react.navigation.link';
-import { reactRouterAdapter } from '@teambit/ui-foundation.ui.navigation.react-router.routing-adapter';
+import { reactRouterAdapter } from '@teambit/ui-foundation.ui.navigation.react-router-adapter';
 import { CommunityHighlighter } from '@teambit/community.ui.community-highlighter';
 
 export function AppContext({ children }: { children?: ReactNode }) {
