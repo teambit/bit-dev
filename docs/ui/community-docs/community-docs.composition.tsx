5036f25993e3cc93096ff11fd6702fbe46a4678c
Kutner JS <urikutner@gmail.com>
2022-01-31 13:45:47 +0200

    remove deprecated react router components (#206)
    * remove deprecated link components
    * get rid of any left over react-router or legacy components

--- docs/ui/community-docs/community-docs.composition.tsx
+++ docs/ui/community-docs/community-docs.composition.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import { RoutingProvider } from '@teambit/base-ui.routing.routing-provider';
-import { useLocation, MemoryRouter } from 'react-router-dom';
-import { Link } from '@teambit/ui-foundation.ui.react-router.link';
-import { NavLink } from '@teambit/ui-foundation.ui.react-router.nav-link';
+import { NavigationProvider } from '@teambit/base-react.navigation.link';
+import { reactRouterAdapter } from '@teambit/ui-foundation.ui.navigation.react-router-adapter';
 import { CommunityDocs } from './community-docs';
 
-const routing = { Link, NavLink, useLocation };
-
 export const BasicCommunityDocs = () => (
-  <RoutingProvider value={routing}>
-    <MemoryRouter>
-      <CommunityDocs />
-    </MemoryRouter>
-  </RoutingProvider>
+  <NavigationProvider implementation={reactRouterAdapter}>
+    <CommunityDocs />
+  </NavigationProvider>
 );
