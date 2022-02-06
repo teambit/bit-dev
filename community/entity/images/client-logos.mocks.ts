b9afc83bb712d210d4f98075390b681f1440f3d0
Josh Kuttler <yoshuakuttler@gmail.com>
2022-02-06 17:45:58 +0200

    change enterprise logo

--- community/entity/images/client-logos.mocks.ts
+++ community/entity/images/client-logos.mocks.ts
@@ -1,34 +1,34 @@
 export const clientLogos = [
   {
     src: 'https://static.bit.dev/homepage-enterprise-logos/Dell.svg',
-    alt: 'Dell'
+    alt: 'Dell',
   },
   {
     src: 'https://static.bit.dev/homepage-enterprise-logos/ebay.svg',
-    alt: 'ebay'
+    alt: 'ebay',
   },
   {
     src: 'https://static.bit.dev/homepage-enterprise-logos/Tesla.svg',
-    alt: 'Tesla'
+    alt: 'Tesla',
   },
   {
     src: 'https://static.bit.dev/homepage-enterprise-logos/at&t.svg',
-    alt: 'AT&T'
+    alt: 'AT&T',
   },
   {
     src: 'https://static.bit.dev/homepage-enterprise-logos/Moodys.svg',
-    alt: 'Moodys'
+    alt: 'Moodys',
   },
   {
     src: 'https://static.bit.dev/homepage-enterprise-logos/Carlsberg-1.svg',
-    alt: 'Carlsberg'
+    alt: 'Carlsberg',
   },
   {
     src: 'https://static.bit.dev/homepage-enterprise-logos/Autodesk.svg',
-    alt: 'Autodesk'
+    alt: 'Autodesk',
   },
   {
-    src: 'https://static.bit.dev/homepage-enterprise-logos/progressive-leasing.svg',
-    alt: 'Progressive Leasing'
-  }
+    src: 'https://static.bit.dev/homepage-enterprise-logos/western-union.svg',
+    alt: 'Western Union',
+  },
 ];
