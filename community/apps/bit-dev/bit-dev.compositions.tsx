f1e896d0bcf96be7f550cee0885e3138e1489c21
Uri Kutner <urikutner@gmail.com>
2022-02-09 22:57:03 +0200

    remove no longer needed MemoryRouter

--- community/apps/bit-dev/bit-dev.compositions.tsx
+++ community/apps/bit-dev/bit-dev.compositions.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
 import { BitDevApp } from './app';
 
 export const BitDevBasic = () => {
-  return (
-    <MemoryRouter>
-      <BitDevApp />
-    </MemoryRouter>
-  );
+  return <BitDevApp />;
 };
