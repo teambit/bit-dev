382cb2d458a6bceb415b0070c3caefe8fc884858
Uri Kutner <urikutner@gmail.com>
2022-02-10 14:47:17 +0200

    leftovers

--- community/content/getting-started/composing/create-components/create-components.composition.tsx
+++ community/content/getting-started/composing/create-components/create-components.composition.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { ThemeCompositions } from '@teambit/documenter.theme.theme-compositions';
-import { MDXLayout } from '@teambit/mdx.ui.mdx-layout';
 import { CreateComponents } from './index';
 
 export const BasicCreateComponents = () => <CreateComponents />;
