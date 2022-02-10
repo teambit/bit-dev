952557d70bc99fc001789c5c02aa8030191948e2
Kutner JS <urikutner@gmail.com>
2022-02-10 19:05:04 +0200

    Replace legacy theme (#220)
    * base components with teambit.react/react env
    
    * checkout latest
    
    * add theme and navigation to compositions
    
    * remove no longer needed MemoryRouter
    
    * leftovers
    
    * checkout latest, fix theme shadow warning
    
    * fix highlighter color
    
    * fix test
    
    * use theme Switcher instead of light theme
    
    * remove mdx templates from community react
    
    * tag community react

--- wiki/sections/wiki/wiki.spec.tsx
+++ wiki/sections/wiki/wiki.spec.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import { BasicWiki } from './wiki.composition';
 
 it('should render with the correct text', () => {
-  const { getByText } = render(<BasicWiki />);
+  const { getByText } = render(
+    <MemoryRouter>
+      <BasicWiki />
+    </MemoryRouter>
+  );
   const rendered = getByText('Scaling in Components');
   expect(rendered).toBeTruthy();
 });
 
 it('should display the welcome page on the root ("") path', () => {
-  const { getByText } = render(<BasicWiki />);
+  const { getByText } = render(
+    <MemoryRouter>
+      <BasicWiki />
+    </MemoryRouter>
+  );
   const rendered = getByText('Welcome to .bit');
   expect(rendered).toBeTruthy();
 });
