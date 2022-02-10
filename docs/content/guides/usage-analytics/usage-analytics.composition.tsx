ebea76fbf0348b0ae5aa9dd996d211e1954c3558
Ran Mizrahi <me@ranmizrahi.com>
2022-02-10 19:36:16 +0200

    updates

--- docs/content/guides/usage-analytics/usage-analytics.composition.tsx
+++ docs/content/guides/usage-analytics/usage-analytics.composition.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import { ThemeCompositions } from '@teambit/documenter.theme.theme-compositions';
 import { MDXLayout } from '@teambit/mdx.ui.mdx-layout';
-// import { BitDevMock } from '@teambit/community.ui.mocks.bit-dev';
 import { UsageAnalytics } from './index';
-
-// export const DisplayedInBitDev = () => <BitDevMock component={<UsageAnalytics />} />;
 
 export const BasicUsageAnalytics = () => {
   return (
--- wiki/apps/wiki/index.ts
+++
@@ -1,1 +0,0 @@
-export { WikiApp } from './app';
