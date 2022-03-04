f081a51c4e650308160ebf7a1ea3570c122fd22e
Kutner JS <urikutner@gmail.com>
2022-03-04 12:38:03 +0200

    highlighter for mdx components (#235)
    * fix highlighter for mdx components
    * add mdx components to preview

--- community/envs/community-mdx/community-mdx.preview.runtime.tsx
+++ community/envs/community-mdx/community-mdx.preview.runtime.tsx
@@ -3,12 +3,18 @@ import { MemoryRouter, UNSAFE_LocationContext } from 'react-router-dom';
 import { PreviewRuntime } from '@teambit/preview';
 import { ReactAspect, ReactPreview } from '@teambit/react';
 import { MDXLayout } from '@teambit/mdx.ui.mdx-layout';
+import type { MDXProviderComponents } from '@teambit/mdx.ui.mdx-layout';
 
 import { ThemeSwitcher } from '@teambit/design.themes.theme-toggler';
-import { NavigationProvider } from '@teambit/base-react.navigation.link';
+import { NavigationProvider, Link, LinkProps } from '@teambit/base-react.navigation.link';
 import { reactRouterAdapter } from '@teambit/ui-foundation.ui.navigation.react-router-adapter';
 
 import { CommunityMdxAspect } from './community-mdx.aspect';
+
+const mdxComponents: MDXProviderComponents = {
+  wrapper: 'div',
+  a: (props: LinkProps) => <Link {...props} external />,
+};
 
 export class CommunityMdxPreview {
   static runtime = PreviewRuntime;
@@ -18,7 +24,7 @@ export class CommunityMdxPreview {
   static async provider([react]: [ReactPreview]) {
     const communityMdxPreview = new CommunityMdxPreview();
     react.registerProvider([
-      MDXLayout,
+      (props: { children: React.ReactNode }) => <MDXLayout {...props} components={mdxComponents} />,
       (props: { children: React.ReactNode }) => <NavigationProvider {...props} implementation={reactRouterAdapter} />,
       (props: { children: React.ReactNode }) => {
         // TODO - should only apply the providers from the current env
