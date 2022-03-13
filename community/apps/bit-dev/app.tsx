009b9c3d7951f2936fe1014bc282d2f9f9fc6b02
Uri Kutner <urikutner@gmail.com>
2022-03-13 18:59:15 +0200

    fix aspects tab

--- community/apps/bit-dev/app.tsx
+++ community/apps/bit-dev/app.tsx
@@ -1,50 +1,41 @@
-import React, { Suspense } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import { lazy } from "@loadable/component";
-import { GoogleTagManager } from "@teambit/analytics.data.google-tag-manager";
-import { GoogleAnalytics } from "@teambit/analytics.data.google-analytics";
-import { Guides } from "@teambit/docs.ui.pages.guides";
-import { Header } from "@teambit/community.ui.header.header";
-import { StickyBanner } from "@teambit/community.ui.sticky-banner";
-import { Homepage } from "@teambit/community.ui.pages.homepage";
-import { NotFound } from "@teambit/community.ui.pages.errors.not-found";
-import { CommunityDocs } from "@teambit/docs.ui.community-docs";
-import { wideColumn } from "@teambit/base-ui.layout.page-frame";
-import { Footer, footerMock } from "@teambit/community.ui.footer.footer";
-import { AppContext } from "./app-context";
+import React, { Suspense } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { lazy } from '@loadable/component';
+import { GoogleTagManager } from '@teambit/analytics.data.google-tag-manager';
+import { GoogleAnalytics } from '@teambit/analytics.data.google-analytics';
+import { Guides } from '@teambit/docs.ui.pages.guides';
+import { Header } from '@teambit/community.ui.header.header';
+import { StickyBanner } from '@teambit/community.ui.sticky-banner';
+import { Homepage } from '@teambit/community.ui.pages.homepage';
+import { NotFound } from '@teambit/community.ui.pages.errors.not-found';
+import { CommunityDocs } from '@teambit/docs.ui.community-docs';
+import { wideColumn } from '@teambit/base-ui.layout.page-frame';
+import { Footer, footerMock } from '@teambit/community.ui.footer.footer';
+import { AppContext } from './app-context';
 
 /**
  * Load pages dynamically to enable code splitting.
  */
-const Plugins = lazy(() => import("@teambit/community.ui.pages.plugins"));
+const Plugins = lazy(() => import('@teambit/community.ui.pages.plugins'));
 
 export function BitDevApp() {
   return (
     <AppContext>
       <GoogleAnalytics trackingId="UA-89811062-3" />
-      <GoogleTagManager gtmIds={["GTM-M99BCWV", "GTM-W637HJ5"]} />
+      <GoogleTagManager gtmIds={['GTM-M99BCWV', 'GTM-W637HJ5']} />
       <StickyBanner />
       <Header />
       <Routes>
         <Route path="/" element={<Homepage />} />
 
-        <Route
-          path="docs"
-          element={<Navigate replace to="/docs/quick-start" />}
-        />
-        <Route
-          path="docs/*"
-          element={<CommunityDocs className={wideColumn} />}
-        />
+        <Route path="docs" element={<Navigate replace to="/docs/quick-start" />} />
+        <Route path="docs/*" element={<CommunityDocs className={wideColumn} />} />
 
-        <Route
-          path="/guides"
-          element={<Navigate replace to="/guides/micro-frontends/overview" />}
-        />
+        <Route path="/guides" element={<Navigate replace to="/guides/micro-frontends/overview" />} />
         <Route path="/guides/*" element={<Guides className={wideColumn} />} />
 
         <Route
-          path="/plugins"
+          path="/aspects"
           element={
             <Suspense fallback={<div />}>
               <Plugins />
