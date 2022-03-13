b0f2408664fc48cdc639949111c3d7ba607ed2cf
Uri Kutner <urikutner@gmail.com>
2022-03-13 16:42:01 +0200

    checkout latest

--- community/entity/graph/bubble-graph/bubble-graph.mock.ts
+++ community/entity/graph/bubble-graph/bubble-graph.mock.ts
@@ -7,10 +7,10 @@ export function mockBubbleGraph() {
      * Highlighted hero components graph
      */
     {
-      id: 'teambit.community/ui/homepage/hero@1.0.1',
+      id: 'teambit.community/ui/hero@1.17.0',
       dependencies: [
-        'teambit.community/entities/hero-graph',
-        'teambit.community/ui/content/heading',
+        'teambit.community/entity/hero-graph',
+        'teambit.community/ui/heading',
         {
           id: 'teambit.design/ui/content/subtitle',
           edge: {
@@ -48,8 +48,8 @@ export function mockBubbleGraph() {
       position: 'top-right',
     },
     {
-      id: 'teambit.community/ui/homepage/homepage@1.1.2',
-      dependencies: ['teambit.community/ui/homepage/hero'],
+      id: 'teambit.community/ui/pages/homepage@1.17.0',
+      dependencies: ['teambit.community/ui/hero'],
       payload: {
         icon: 'https://static.bit.dev/brands/logo-react.svg',
       },
@@ -72,8 +72,8 @@ export function mockBubbleGraph() {
       position: 'top-right',
     },
     {
-      id: 'teambit.community/apps/website@1.2.1',
-      dependencies: ['teambit.community/ui/homepage/homepage'],
+      id: 'teambit.community/apps/bit-dev@1.95.7',
+      dependencies: ['teambit.community/ui/pages/homepage'],
       payload: {
         icon: 'https://static.bit.dev/brands/logo-react.svg',
       },
@@ -99,7 +99,7 @@ export function mockBubbleGraph() {
       id: 'teambit.design/ui/content/heading@2.2.1',
       dependencies: [
         {
-          id: 'teambit.community/ui/content/heading',
+          id: 'teambit.community/ui/heading',
           edge: {
             direction: Direction.TAIL,
           },
@@ -123,9 +123,9 @@ export function mockBubbleGraph() {
       position: 'right',
     },
     {
-      id: 'teambit.react-base/buttons/button@2.0.3',
+      id: 'teambit.base-react/buttons/button@2.0.3',
       dependencies: [
-        'teambit.react-base/navigation/link',
+        'teambit.base-react/navigation/link',
         {
           id: 'teambit.design/ui/buttons/button',
           edge: {
@@ -155,7 +155,7 @@ export function mockBubbleGraph() {
       position: 'top-right',
     },
     {
-      id: 'teambit.react-base/navigation/link@1.2.9',
+      id: 'teambit.base-react/navigation/link@1.2.9',
       dependencies: [],
       payload: {
         icon: 'https://static.bit.dev/brands/logo-react.svg',
@@ -178,7 +178,7 @@ export function mockBubbleGraph() {
       },
     },
     {
-      id: 'teambit.community/entities/hero-graph@1.3.1',
+      id: 'teambit.community/entity/hero-graph@1.3.1',
       dependencies: [],
       payload: {
         icon: 'https://static.bit.dev/brands/logo-nodejs.svg',
@@ -209,7 +209,7 @@ export function mockBubbleGraph() {
       id: 'teambit.workspace/workspace@1.4.1',
       dependencies: [
         'teambit.component/component',
-        'teambit.ui-foundation/ui/component-tree',
+        'teambit.ui-foundation/ui/tree/file-tree',
         'teambit.ui-foundation/ui/top-bar',
       ],
       payload: {
@@ -227,7 +227,10 @@ export function mockBubbleGraph() {
     },
     {
       id: 'teambit.scope/scope@1.3.1',
-      dependencies: ['teambit.component/component', 'teambit.ui-foundation/ui/top-bar'],
+      dependencies: [
+        'teambit.component/component',
+        'teambit.ui-foundation/ui/top-bar',
+      ],
       payload: {
         icon: 'https://static.bit.dev/Community/icons/collection.svg',
       },
@@ -258,7 +261,7 @@ export function mockBubbleGraph() {
       position: 'top-right',
     },
     {
-      id: 'teambit.ui-foundation/ui/component-tree@1.2.1',
+      id: 'teambit.ui-foundation/ui/tree/file-tree@1.2.1',
       dependencies: [],
       payload: {
         icon: 'https://static.bit.dev/brands/logo-react.svg',
@@ -275,6 +278,7 @@ export function mockBubbleGraph() {
     },
     {
       id: 'teambit.ui-foundation/ui/top-bar@1.0.1',
+      isLinkable: false,
       dependencies: [],
       payload: {
         icon: 'https://static.bit.dev/brands/logo-react.svg',
@@ -307,6 +311,7 @@ export function mockBubbleGraph() {
     },
     {
       id: 'teambit.compilation/babel@1.1.0',
+      isLinkable: false,
       dependencies: ['teambit.compilation/compiler'],
       // TODO: add babel icon
       payload: {
@@ -339,7 +344,7 @@ export function mockBubbleGraph() {
       position: 'top-right',
     },
     {
-      id: 'teambit.tester/tester@1.2.1',
+      id: 'teambit.defender/tester@1.2.1',
       dependencies: [],
       payload: {
         icon: 'https://static.bit.dev/Community/icons/tester.svg',
@@ -355,8 +360,8 @@ export function mockBubbleGraph() {
       position: 'bottom-right',
     },
     {
-      id: 'teambit.linting/linter@1.2.1',
-      dependencies: ['teambit.tester/eslint'],
+      id: 'teambit.defender/linter@1.2.1',
+      dependencies: ['teambit.defender/eslint'],
       payload: {
         icon: 'https://static.bit.dev/Community/icons/linter.svg',
       },
@@ -371,7 +376,8 @@ export function mockBubbleGraph() {
       position: 'bottom-right',
     },
     {
-      id: 'teambit.tester/eslint@1.2.1',
+      id: 'teambit.defender/eslint@1.2.1',
+      isLinkable: false,
       dependencies: [],
       payload: {
         icon: 'https://static.bit.dev/Community/icons/esling.svg',
@@ -388,7 +394,11 @@ export function mockBubbleGraph() {
     },
     {
       id: 'teambit.react/react@2.0.1',
-      dependencies: ['teambit.typescript/typescript', 'teambit.tester/jest', 'teambit.tester/eslint'],
+      dependencies: [
+        'teambit.typescript/typescript',
+        'teambit.defender/jest',
+        'teambit.defender/eslint',
+      ],
       payload: {
         icon: 'https://static.bit.dev/Community/icons/reactjs-puzzle.svg',
       },
@@ -403,8 +413,9 @@ export function mockBubbleGraph() {
       position: 'bottom-right',
     },
     {
-      id: 'teambit.tester/jest@3.2.1',
-      dependencies: ['teambit.tester/tester'],
+      id: 'teambit.defender/jest@3.2.1',
+      isLinkable: false,
+      dependencies: ['teambit.defender/tester'],
       payload: {
         icon: 'https://static.bit.dev/brands/logo-nodejs.svg',
       },
@@ -423,6 +434,7 @@ export function mockBubbleGraph() {
      */
     {
       id: 'learn-bit-angular.ecommerce/store@1.3.1',
+      isLinkable: false,
       dependencies: [
         'learn-bit-angular.ecommerce/cart',
         'learn-bit-angular.ecommerce/product',
@@ -443,6 +455,7 @@ export function mockBubbleGraph() {
     },
     {
       id: 'learn-bit-angular.ecommerce/cart@1.3.1',
+      isLinkable: false,
       dependencies: [],
       payload: {
         icon: 'https://static.bit.dev/brands/logo-angular.svg',
@@ -459,6 +472,7 @@ export function mockBubbleGraph() {
     },
     {
       id: 'learn-bit-angular.ecommerce/hero@1.1.5',
+      isLinkable: false,
       dependencies: [],
       payload: {
         icon: 'https://static.bit.dev/brands/logo-angular.svg',
@@ -475,6 +489,7 @@ export function mockBubbleGraph() {
     },
     {
       id: 'learn-bit-angular.ecommerce/product@1.3.1',
+      isLinkable: false,
       dependencies: [],
       payload: {
         icon: 'https://static.bit.dev/brands/logo-angular.svg',
@@ -509,7 +524,7 @@ export function mockBubbleGraph() {
       position: 'bottom-right',
     },
     {
-      id: 'teambit.mdx/babel/mdx-loader@3.1.2',
+      id: 'teambit.mdx/modules/mdx-loader@3.1.2',
       dependencies: ['teambit.mdx/modules/mdx-compiler'],
       payload: {
         icon: 'https://static.bit.dev/brands/logo-nodejs.svg',
@@ -544,9 +559,10 @@ export function mockBubbleGraph() {
      * Data Fetching, React and Angular (composed)
      */
     {
-      id: 'teambit.mdx/ui/pages/book-page@0.0.2',
+      id: 'learn-bit-react.data-fetching/ui/pages/book-page@0.0.2',
+      isLinkable: false,
       dependencies: [
-        'teambit.mdx/ui/book-list',
+        'learn-bit-react.data-fetching/ui/book-list',
         'teambit.frontend/ui/angular-renderer',
         {
           id: 'learn-bit-react.angular-in-react/books/recommendations',
@@ -571,6 +587,7 @@ export function mockBubbleGraph() {
     },
     {
       id: 'teambit.frontend/ui/angular-renderer@0.0.2',
+      isLinkable: false,
       dependencies: [],
       payload: {
         icon: 'https://static.bit.dev/brands/logo-react.svg',
@@ -587,6 +604,7 @@ export function mockBubbleGraph() {
     },
     {
       id: 'learn-bit-react.angular-in-react/books/recommendations@0.0.2',
+      isLinkable: false,
       dependencies: [],
       payload: {
         icon: 'https://static.bit.dev/brands/logo-angular.svg',
@@ -602,8 +620,11 @@ export function mockBubbleGraph() {
       position: 'top-right',
     },
     {
-      id: 'teambit.mdx/ui/book-list@0.0.2',
-      dependencies: ['teambit.mdx/ui/hooks/use-book', 'learn-bit-react.data-fetching/models/book'],
+      id: 'learn-bit-react.data-fetching/ui/book-list@0.0.2',
+      dependencies: [
+        'learn-bit-react.data-fetching/ui/hooks/use-books',
+        'learn-bit-react.data-fetching/models/book',
+      ],
       payload: {
         icon: 'https://static.bit.dev/brands/logo-react.svg',
       },
@@ -618,7 +639,7 @@ export function mockBubbleGraph() {
       position: 'bottom-right',
     },
     {
-      id: 'teambit.mdx/ui/hooks/use-book@0.0.2',
+      id: 'learn-bit-react.data-fetching/ui/hooks/use-books@0.0.2',
       dependencies: ['learn-bit-react.data-fetching/models/book'],
       payload: {
         icon: 'https://static.bit.dev/brands/logo-react.svg',
--- community/ui/graph/component-bubble/component-bubble.tsx
+++ community/ui/graph/component-bubble/component-bubble.tsx
@@ -4,7 +4,11 @@ import { Image } from '@teambit/base-react.content.image';
 import { Label } from '@teambit/documenter.ui.label';
 import { Caption } from '@teambit/design.ui.content.caption';
 import classNames from 'classnames';
-import { BubbleCard, BubbleCardProps } from '@teambit/design.ui.cards.bubble-card';
+import {
+  BubbleCard,
+  BubbleCardProps,
+} from '@teambit/design.ui.cards.bubble-card';
+import { ComponentUrl } from '@teambit/component.modules.component-url';
 import { Ellipsis, ellipsis } from '@teambit/design.ui.styles.ellipsis';
 import { getScopeName } from './get-scope-name';
 import styles from './component-bubble.module.scss';
@@ -49,6 +53,10 @@ export type ComponentBubbleProps = {
    * classname to inject the element.
    */
   className?: string;
+  /**
+   * link the component bubble to its component page
+   */
+  isLinkable?: boolean;
 
   showScope?: boolean;
 } & BubbleCardProps;
@@ -63,31 +71,51 @@ export function ComponentBubble({
   allowHover = true,
   forceActive = false,
   nonInteractive = false,
+  isLinkable = true,
   ...rest
 }: ComponentBubbleProps) {
   if (nonInteractive) {
     showVersion = false;
     allowHover = false;
   }
-
+  const isLinked = componentId && isLinkable;
   return (
-    <BubbleCard
-      className={classNames(styles.bubble, className, forceActive && styles.active, allowHover && styles.interactive)}
-      {...rest}
+    <a
+      href={
+        isLinked
+          ? ComponentUrl.toUrl(componentId as any, { includeVersion: false })
+          : 'javascript:void(0)'
+      }
+      target={isLinked ? '_blank' : '_self'}
     >
-      {icon && <Image src={icon} className={styles.icon} />}
-      {componentId && (
-        <div className={classNames(styles.id)}>
-          <Caption className={ellipsis}>
-            {showScope
-              ? `${getScopeName(componentId.scope, showOwner)}/${componentId.namespace}`
-              : componentId.namespace}
-          </Caption>
-          <Ellipsis className={styles.name}>{componentId.name}</Ellipsis>
-        </div>
-      )}
-      {componentId?.version && showVersion && <Label className={styles.versionLabel}>{componentId.version}</Label>}
-    </BubbleCard>
+      <BubbleCard
+        className={classNames(
+          styles.bubble,
+          className,
+          forceActive && styles.active,
+          allowHover && styles.interactive,
+          !isLinked && styles.noLink
+        )}
+        {...rest}
+      >
+        {icon && <Image src={icon} className={styles.icon} />}
+        {componentId && (
+          <div className={classNames(styles.id)}>
+            <Caption className={ellipsis}>
+              {showScope
+                ? `${getScopeName(componentId.scope, showOwner)}/${
+                    componentId.namespace
+                  }`
+                : componentId.namespace}
+            </Caption>
+            <Ellipsis className={styles.name}>{componentId.name}</Ellipsis>
+          </div>
+        )}
+        {componentId?.version && showVersion && (
+          <Label className={styles.versionLabel}>{componentId.version}</Label>
+        )}
+      </BubbleCard>
+    </a>
   );
 }
 
