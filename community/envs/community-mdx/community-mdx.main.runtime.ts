33bca0946c9273f62b442919046538bd9b37b1b6
Ran Mizrahi <me@ranmizrahi.com>
2022-03-14 19:18:47 +0200

    chnages

--- community/envs/community-mdx/community-mdx.main.runtime.ts
+++ community/envs/community-mdx/community-mdx.main.runtime.ts
@@ -23,7 +23,9 @@ export class CommunityMdxMain {
   static async provider([envs, mdxMain, generator]: [EnvsMain, MDXMain, GeneratorMain]) {
     const { devDependencies, dependencies }: any = mdxMain.mdxEnv.getDependencies();
     const deps = {
-      'core-js': dependencies['core-js'],
+      ...dependencies,
+      '@teambit/mdx.ui.mdx-scope-context': '0.0.368',
+      '@mdx-js/react': '1.6.22',
     };
     const communityMdxEnv = envs.compose(mdxMain.mdxEnv, [
       envs.override({
