0a114ce8e88c8e83da6c37cd21cdf08bf58cc17a
oded <oded@bit.dev>
2022-03-22 13:03:36 +0200

    toc- update content when moving between pages + scroll to active link on first load

--- docs/ui/navigation/table-of-content/table-of-content.tsx
+++ docs/ui/navigation/table-of-content/table-of-content.tsx
@@ -1,7 +1,7 @@
-import React, { useMemo, ReactNode, useEffect } from 'react';
+import React, { useMemo, ReactNode, useEffect, useState } from 'react';
 import classNames from 'classnames';
 import { classes } from '@teambit/design.ui.surfaces.menu.item';
-import { Link } from '@teambit/base-react.navigation.link';
+import { Link, useLocation } from '@teambit/base-react.navigation.link';
 import { useElementOnFold } from '@teambit/docs.ui.hooks.use-element-on-fold';
 import styles from './table-of-content.module.scss';
 
@@ -22,20 +22,40 @@ export type TableOfContentProps = {
 
 export function TableOfContent({ className, children, title, rootRef, selectors, ...rest }: TableOfContentProps) {
   const { activeElement, elements } = useElementOnFold(rootRef, selectors);
+  const [loaded, setLoaded] = useState(false);
+  const [key, setKey] = useState(0);
+  const currentLocation = useLocation();
   const anchors = useMemo(() => getLinks(elements), [elements]);
+
+  useEffect(() => {
+    // identifies first load
+    if(!loaded && elements.length > 0) {
+      setLoaded(!loaded)
+    } else {
+      // makes sure to remove stale content
+      setKey(key + 1)
+    }
+  }, [elements])
+  
+  // scroll to active link, after first load and after content is loaded
+  useEffect(() => {
+    const hash = currentLocation?.hash?.replace('#', '')
+    const el = document.getElementById(`link-${hash}`)
+    el?.scrollIntoView();
+  }, [loaded]);
 
   if (anchors.length < 1) return null;
 
   return (
-    <div {...rest} className={classNames(styles.tableOfContent, className)}>
+    <div {...rest} key={key} className={classNames(styles.tableOfContent, className)}>
       {title && <div className={styles.title}>{title}</div>}
       {anchors.map((link) => {
         const isActive = activeElement === link?.actualElement;
-
         return (
           <Link
             native
             key={link?.id}
+            id={`link-${link?.id || ''}`}
             className={classNames(styles.item, classes.menuItem, classes.interactive, isActive && classes.active)}
             href={`#${link?.id}`}
             data-element-type={link?.elementType}
