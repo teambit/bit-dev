cdcafc06d0311ee9e3815d5fe8014f8ded16ae61
Ran Mizrahi <me@ranmizrahi.com>
2022-03-27 19:23:45 +0300

    merged updates

--- component/content/component-main-file/component-main-file.composition.tsx
+++ component/content/component-main-file/component-main-file.composition.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import { ThemeCompositions } from '@teambit/documenter.theme.theme-compositions';
+import { MDXLayout } from '@teambit/mdx.ui.mdx-layout';
 import { ComponentMainFile } from './index';
 
-export const BasicComponentMainFile = () => (
-  <ComponentMainFile />
+export const Content = () => (
+  <ThemeCompositions>
+    <MDXLayout>
+      <base href="https://bit.dev/docs/" target="_blank" />
+      <ComponentMainFile />
+    </MDXLayout>
+  </ThemeCompositions>
 );
