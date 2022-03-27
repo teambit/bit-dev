c16468499d694784a7c7ab713fc651fddc02c2e0
Kutner JS <urikutner@gmail.com>
2022-03-27 19:06:32 +0300

    run bubble graph updater in a continuous interval (#344)
    * run bubble graph updater in a continuous interval
    
    * interval all
    
    Co-authored-by: Ran Mizrahi <ranm8@users.noreply.github.com>

--- community/ui/graph/edge/auto-reloader.tsx
+++ community/ui/graph/edge/auto-reloader.tsx
@@ -1,28 +1,37 @@
 import React, { PropsWithChildren, useEffect } from 'react';
 import { Xwrapper, useXarrow } from 'react-xarrows';
 
-export type ArrowAutoReloaderProps = PropsWithChildren<{}>;
+export type ReloadOptions = { delay?: number; interval?: boolean };
+export type ArrowAutoReloaderProps = PropsWithChildren<ReloadOptions>;
 
 /**
  * automatically reloads arrows on mount
  * this solves some glitches on first time rendering
  */
-export function ArrowAutoReloader({ children }: ArrowAutoReloaderProps) {
+export function ArrowAutoReloader({ children, delay, interval }: ArrowAutoReloaderProps) {
   return (
     <Xwrapper>
       {children}
-      <ReloadArrows />
+      <ReloadArrows delay={delay} interval={interval} />
     </Xwrapper>
   );
 }
 
 // this will be fixed in react-xarrows v3.0.0
-function ReloadArrows() {
+function ReloadArrows({ delay = 1000, interval = false }: ReloadOptions) {
   const updateXarrow = useXarrow();
 
   useEffect(() => {
     updateXarrow();
-  }, []);
+
+    if (interval) {
+      const tid = setInterval(updateXarrow, delay);
+      return () => clearInterval(tid);
+    }
+
+    const tid = setTimeout(updateXarrow, delay);
+    return () => clearTimeout(tid);
+  }, [delay, interval]);
 
   return null;
 }
