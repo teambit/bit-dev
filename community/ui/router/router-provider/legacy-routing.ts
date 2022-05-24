47bc7c7155f326a07d62087abfcee1d96de9c392
Uri Kutner <urikutner@gmail.com>
2022-05-24 20:50:36 +0300

    fox types

--- community/ui/router/router-provider/legacy-routing.ts
+++ community/ui/router/router-provider/legacy-routing.ts
@@ -4,7 +4,9 @@ import type { Routing } from '@teambit/base-ui.routing.routing-provider';
 import { reactRouterAdapter } from '@teambit/ui-foundation.ui.navigation.react-router-adapter';
 
 export const legacyRouting: Routing = {
+  // @ts-ignore
   Link: reactRouterAdapter.Link!,
+  // @ts-ignore
   NavLink: reactRouterAdapter.Link!,
   // @ts-ignore
   useLocation: reactRouterAdapter.useLocation,
