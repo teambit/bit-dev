61500737f1f683456186d82cacd9b3b54ace7d56
Gilad Shoham <shoham.gilad@gmail.com>
2022-05-29 19:00:30 +0300

    replace gql alias + expose loader to use cjs instead of esm

--- community/envs/community-react/webpack/webpack-transformers.ts
+++ community/envs/community-react/webpack/webpack-transformers.ts
@@ -1,45 +1,81 @@
+import { WebpackConfigTransformer, WebpackConfigMutator, WebpackConfigTransformContext } from '@teambit/webpack';
 
-  import { WebpackConfigTransformer, WebpackConfigMutator, WebpackConfigTransformContext } from '@teambit/webpack';
+/**
+ * Transformation to apply for both preview and dev server
+ * @param config
+ * @param _context
+ */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function commonTransformation(config: WebpackConfigMutator, _context: WebpackConfigTransformContext) {
+  // Merge config with the webpack.config.js file if you choose to import a module export format config.
+  // config.merge([webpackConfig]);
 
-  /**
-   * Transformation to apply for both preview and dev server
-   * @param config
-   * @param _context
-   */
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  function commonTransformation(config: WebpackConfigMutator, _context: WebpackConfigTransformContext) {
-    // Merge config with the webpack.config.js file if you choose to import a module export format config.
-    // config.merge([webpackConfig]);
-    // config.addAliases({});
-    // config.addModuleRule(youRuleHere);
-    return config;
+  // config.addModuleRule(youRuleHere);
+  return config;
+}
+
+export const replaceGqlExposeTransformer: WebpackConfigTransformer = (config: WebpackConfigMutator) => {
+  if (config?.raw?.module?.rules) {
+    // eslint-disable-next-line no-param-reassign
+    const gqlRule = config.raw.module.rules.find((rule) => {
+      // prettier-ignore
+
+      return !!(
+          (
+            // @ts-ignore
+            rule.loader?.includes('expose-loader') &&
+            // @ts-ignore
+            typeof rule.test &&
+            // @ts-ignore
+            typeof rule.test === 'string' &&
+            // @ts-ignore
+            rule.test?.includes('/graphql/')
+          )
+        );
+    });
+    if (gqlRule) {
+      // @ts-ignore
+      gqlRule.test = require.resolve('graphql');
+    }
   }
-
-  /**
-   * Transformation for the preview only
-   * @param config
-   * @param context
-   * @returns
-   */
-  export const previewConfigTransformer: WebpackConfigTransformer = (
-    config: WebpackConfigMutator,
-    context: WebpackConfigTransformContext
-  ) => {
-    const newConfig = commonTransformation(config, context);
-    return newConfig;
+  return config;
+};
+export const addGqlAliasTransformer: WebpackConfigTransformer = (config: WebpackConfigMutator) => {
+  // eslint-disable-next-line no-param-reassign
+  config.raw.resolve = config.raw.resolve || {};
+  // eslint-disable-next-line no-param-reassign
+  config.raw.resolve.alias = {
+    graphql$: require.resolve('graphql'),
+    ...config.raw.resolve?.alias,
   };
 
-  /**
-   * Transformation for the dev server only
-   * @param config
-   * @param context
-   * @returns
-   */
-  export const devServerConfigTransformer: WebpackConfigTransformer = (
-    config: WebpackConfigMutator,
-    context: WebpackConfigTransformContext
-  ) => {
-    const newConfig = commonTransformation(config, context);
-    return newConfig;
-  };
+  return config;
+};
 
+/**
+ * Transformation for the preview only
+ * @param config
+ * @param context
+ * @returns
+ */
+export const previewConfigTransformer: WebpackConfigTransformer = (
+  config: WebpackConfigMutator,
+  context: WebpackConfigTransformContext
+) => {
+  const newConfig = commonTransformation(config, context);
+  return newConfig;
+};
+
+/**
+ * Transformation for the dev server only
+ * @param config
+ * @param context
+ * @returns
+ */
+export const devServerConfigTransformer: WebpackConfigTransformer = (
+  config: WebpackConfigMutator,
+  context: WebpackConfigTransformContext
+) => {
+  const newConfig = commonTransformation(config, context);
+  return newConfig;
+};
