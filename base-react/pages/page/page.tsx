969d3c5f6cda8aff9770f67fdbf632ca9a088b6a
Uri Kutner <urikutner@gmail.com>
2022-05-30 20:45:11 +0300

    more meta tags

--- base-react/pages/page/page.tsx
+++ base-react/pages/page/page.tsx
@@ -22,8 +22,16 @@ export function Page({ children, title, description, ...rest }: PageProps) {
   return (
     <div {...rest}>
       <Helmet>
+        {/* only add what is needed */}
         <title>{title}</title>
+        <meta property="og:title" content={title} />
+        <meta property="twitter:title" content={title} />
         <meta name="description" content={description} />
+        <meta name="og:description" content={description} />
+        <meta name="twitter:description" content={description} />
+
+        <meta name="twitter:image" content="https://static.bit.cloud/og-images/bit-marketing-og-img-twitter.png" />
+        <meta property="og:image" content="https://static.bit.cloud/og-images/bit-marketing-og-img-facebook.png" />
       </Helmet>
       {children}
     </div>
