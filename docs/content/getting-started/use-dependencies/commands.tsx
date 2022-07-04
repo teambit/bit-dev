f673d8252ced7299fef86ff5835b629cede62415
Eden <eden@bit.dev>
2022-07-04 16:38:42 +0300

    update use dependencies

--- docs/content/getting-started/use-dependencies/commands.tsx
+++ docs/content/getting-started/use-dependencies/commands.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 // import { Terminal } from '@teambit/community.ui.bit-cli.terminal';
 import { CommandsExplorer } from '@teambit/community.ui.bit-cli.commands-explorer';
 
-export const InstallDependency = () => {
+export const BitInstall = () => <CommandsExplorer commandName="install" />;
+
+export const InstallExample = () => {
   const example = {
     'packages...': '@teambit/design.inputs.input-text',
   };
--- docs/content/getting-started/use-dependencies/expanded-code.tsx
+++ docs/content/getting-started/use-dependencies/expanded-code.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { CollapsibleContent } from '@teambit/design.content.collapsible-content';
+import { BitInstall } from './commands';
 import CodeExample from './code-example.mdx';
 import styles from './styles.module.scss';
 
@@ -8,6 +9,18 @@ export const ExpandedCode = () => (
     <CollapsibleContent
       title={<span>See full code snippet</span>}
       content={<CodeExample />}
+      hasSeperator={false}
+      style={{ paddingTop: 0 }}
+      className={styles.collapsible}
+    />
+  </div>
+);
+
+export const ExpandedCommand = () => (
+  <div className={styles.collapsibleWrapper}>
+    <CollapsibleContent
+      title={<span>See full command options</span>}
+      content={<BitInstall />}
       hasSeperator={false}
       style={{ paddingTop: 0 }}
       className={styles.collapsible}
